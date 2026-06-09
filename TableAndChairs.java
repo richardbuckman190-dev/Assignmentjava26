bf8ff58803289716beeb85a06f0c772346bbeeb4
Richard Buckman <richardbuckman190@gmail.com>
2026-06-09 14:55:04 +0100

    Add files via upload

---
+++ TableAndChairs.java
@@ -0,0 +1,18 @@
+public class TableAndChairs {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("x                                       x");
+        System.out.println("x                                       x");
+        System.out.println("x                                       x");
+        System.out.println("xxxx         xxxxxxxxxxxxx           xxxx");
+        System.out.println("x   x        x           x           x  x");
+        System.out.println("x   x        x           x           x  x");
+        System.out.println("x   x        x           x           x  x");
+        System.out.println("x   x        x           x           x  x");
+
+    }
+
+
+}
