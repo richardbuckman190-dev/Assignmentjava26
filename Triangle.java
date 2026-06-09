850d2401f6176743c894adfa08900488e7f560a1
Richard Buckman <richardbuckman190@gmail.com>
2026-06-09 15:23:34 +0100

    Add files via upload

---
+++ Triangle.java
@@ -0,0 +1,17 @@
+  public class Triangle{
+
+   public static void main(String[] args) {
+
+
+
+
+    
+    System.out.println("                        T");
+    System.out.println("                       TTT");
+    System.out.println("                      TTTTT");
+    System.out.println("                     TTTTTTTT");
+    System.out.println("                    TTTTTTTTTT");
+    System.out.println("                   TTTTTTTTTTTT");
+    System.out.println("                  TTTTTTTTTTTTTT");
+   }
+ }
